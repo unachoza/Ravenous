f2709e07ab80374c2bf9b61448970ecd9e007a33
Arianna Choza <unachoza@gmail.com>
2019-03-13 14:13:21 -0400

    search bar and sort function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Business from './components/Business/Business'
 import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from '../src/components/SearchBar/SearchBar'
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
+        <SearchBar/>
         <BusinessList/>
       </div>
     );
