2d8fbcdb01b8840619655611dde6a0f184bd3e3b
Arianna Choza <unachoza@gmail.com>
2019-03-13 13:18:35 -0400

    business list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Business from './components/Business/Business'
+import BusinessList from './components/BusinessList/BusinessList';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Business/>
+        <BusinessList/>
       </div>
     );
   }
